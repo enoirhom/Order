d7e9aea0754f03d7f38973afaa2649ea7ac193be
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-08 14:53:55 +0100

    Refactor

--- domain/src/main/java/com/switchfully/item/OrderItem.java
+++ domain/src/main/java/com/switchfully/item/OrderItem.java
@@ -3,6 +3,9 @@ package com.switchfully.item;
 import java.time.LocalDate;
 
 public class OrderItem {
+    private static final int NORMAL_SHIPPING_DELAY = 1;
+    private static final int OUT_OF_STOCK_SHIPPING_DELAY = 7;
+
     private final Item item;
     private final int quantity;
     private final double price;
@@ -17,9 +20,9 @@ public class OrderItem {
 
     private LocalDate calculateShippingDate(int stockQuantity, int orderQuantity) {
         if (stockQuantity >= orderQuantity) {
-            return LocalDate.now();
+            return LocalDate.now().plusDays(NORMAL_SHIPPING_DELAY);
         }
-        return LocalDate.now().plusDays(7);
+        return LocalDate.now().plusDays(OUT_OF_STOCK_SHIPPING_DELAY);
     }
 
     public String getId() {
