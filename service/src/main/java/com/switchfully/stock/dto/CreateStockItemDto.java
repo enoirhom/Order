1088e99a71e7bd1ccf557ec8a9e377e2578c8cd7
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-08 14:23:57 +0100

    Refactor

--- service/src/main/java/com/switchfully/stock/dto/CreateStockItemDto.java
+++ service/src/main/java/com/switchfully/stock/dto/CreateStockItemDto.java
@@ -1,4 +1,4 @@
 package com.switchfully.stock.dto;
 
-public record CreateStockItemDto(String name, String description, double price, int quantity) {
+public record CreateStockItemDto(String name, String description, int quantity, double price) {
 }
