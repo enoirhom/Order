a5e2163db2e0a5f0cae75182b3f54eabdd7c84c5
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-24 20:57:22 +0100

    Adding test

---
+++ jar/src/test/java/com/switchfully/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.switchfully;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
